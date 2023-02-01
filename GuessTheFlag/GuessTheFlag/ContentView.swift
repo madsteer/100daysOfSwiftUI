b2a03f2db0d843f1f422366497f4618e6c2a230b
Cory Steers <cory.steers.gmu9@statefarm.com>
2023-02-01 10:41:21 -0600

    project 2: initial setup

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Cory Steers on 1/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var countries = [ "Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russion", "Spain", "UK", "US" ]
+    var correctAnswer = Int.random(in: 0...2)
+    
+    var body: some View {
+        ZStack {
+            Color.blue.ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        // flag was tapped
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Cory Steers on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
