d2edf2327ea37da48e2c05e91134ec4143e22dd3
Cory Steers <cory.steers.gmu9@statefarm.com>
2023-02-16 16:19:39 -0600

    day 23 challenge 3: create custom title ViewModifier

---
+++ Day23Challenge3/Day23Challenge3/Day23Challenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  Day23Challenge3App.swift
+//  Day23Challenge3
+//
+//  Created by Cory Steers on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Day23Challenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
