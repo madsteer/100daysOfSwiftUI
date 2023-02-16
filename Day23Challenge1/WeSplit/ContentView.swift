775b784b0e18ffdab41a7e0a8fca5782df46f941
Cory Steers <cory.steers.gmu9@statefarm.com>
2023-02-16 16:00:00 -0600

    day 23 challenge 1: add conditional text color

---
+++ Day23Challenge1/WeSplit/ContentView.swift
@@ -0,0 +1,115 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Cory Steers on 12/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+
+    let tipPercentages = [10, 15, 20, 25, 0]
+
+    var checkPlusTip: Double  {
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+
+        return grandTotal
+    }
+
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+
+    var localDollarFormat: FloatingPointFormatStyle<Double>.Currency {
+        let codeString = Locale.current.currency?.identifier ?? "USD"
+        return FloatingPointFormatStyle<Double>.Currency( code: codeString)
+    }
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: localDollarFormat)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                } header: {
+                    Text("What is bill total?")
+                }
+
+//segmented style
+//                Section {
+//                    Picker("Tip percentage", selection: $tipPercentage) {
+//                        ForEach(tipPercentages, id: \.self) {
+//                            Text($0, format: .percent)
+//                        }
+//                    }
+//                    .pickerStyle(.segmented)
+//                } header: {
+//                    Text("How much tip do you want to leave?")
+//                }
+
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                        .clipped()
+                        .compositingGroup()
+                    }
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+
+                Section {
+                    Text(checkPlusTip, format: localDollarFormat)
+                } header: {
+                    Text("Amount Plus Tip")
+                        .foregroundColor(tipPercentage != 0 ? .black : .red)
+                }
+
+                Section {
+                    Text(totalPerPerson, format: localDollarFormat)
+                } header: {
+                    Text("Amount person")
+                }
+            }
+            .navigationTitle("weSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day23Challenge1/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Cory Steers on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
