418921beec28460a734213b6b9dcfdc8008432e1
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-03-23 16:15:26 -0500

    day 24, project 4 - initial layout

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Cory Steers on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
