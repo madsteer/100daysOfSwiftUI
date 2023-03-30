f1176e55bb42081259e56e9c531b4529b8d85725
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-03-30 15:09:40 -0500

    project 6 - animating bindings

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,54 +11,25 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap me") {
-//            animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                    .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
-        }
-//        .scaleEffect(animationAmount)
-//        .blur(radius: (animationAmount - 1) * 3)
-//        .animation(.default, value: animationAmount)
-//        .animation(.easeOut, value: animationAmount)
-//        .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
-//        .animation(.easeInOut(duration: 2), value: animationAmount)
-//        .animation(
-//            .easeInOut(duration: 2)
-//            .delay(1),
-//            value: animationAmount
-//        )
-//        .animation(
-//            .easeInOut(duration: 1)
-//            .repeatCount(3, autoreverses: true),
-//            value: animationAmount
-//        )
-//        .animation(
-//            .easeInOut(duration: 1)
-//            .repeatCount(2, autoreverses: true),
-//            value: animationAmount
-//        )
-//        .animation(
-//            .easeInOut(duration: 1)
-//            .repeatForever(autoreverses: true),
-//            value: animationAmount
-//        )
+        print(animationAmount)
         
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+        }
     }
 }
 
