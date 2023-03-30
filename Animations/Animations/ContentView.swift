1631c1570cfc08281c4e8e9910d9e7f800ba5df6
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-03-30 14:59:59 -0500

    project 6 - customizing animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -12,15 +12,53 @@ struct ContentView: View {
     
     var body: some View {
         Button("Tap me") {
-            animationAmount += 1
+//            animationAmount += 1
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
+//        .animation(.default, value: animationAmount)
+//        .animation(.easeOut, value: animationAmount)
+//        .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
+//        .animation(.easeInOut(duration: 2), value: animationAmount)
+//        .animation(
+//            .easeInOut(duration: 2)
+//            .delay(1),
+//            value: animationAmount
+//        )
+//        .animation(
+//            .easeInOut(duration: 1)
+//            .repeatCount(3, autoreverses: true),
+//            value: animationAmount
+//        )
+//        .animation(
+//            .easeInOut(duration: 1)
+//            .repeatCount(2, autoreverses: true),
+//            value: animationAmount
+//        )
+//        .animation(
+//            .easeInOut(duration: 1)
+//            .repeatForever(autoreverses: true),
+//            value: animationAmount
+//        )
+        
     }
 }
 
