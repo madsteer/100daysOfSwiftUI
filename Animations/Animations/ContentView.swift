eab049f76beafd8ea18f60fa79f55b25ae547fc1
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-04-01 17:18:09 -0500

    project 6 - animating gestures

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,27 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var animationAmount = 0.0
-    @State private var enabled = false
+    let letters = Array("Hello, SwiftUI")
+    @State private var enabled = true
+    @State private var dragAmount = CGSize.zero
+    
     
     var body: some View {
-        Button("Tap Me") {
-            enabled.toggle()
-//            withAnimation {
-//                animationAmount += 180
-//            }
-//            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-//                animationAmount += 360
-//            }
+//        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//            .frame(width: 300, height: 200)
+//            .clipShape(RoundedRectangle(cornerRadius: 10))
+//            .offset(dragAmount)
+//            .gesture(
+//                DragGesture()
+//                    .onChanged { dragAmount = $0.translation }
+//                    .onEnded { _ in
+//                        withAnimation {
+//                            dragAmount = .zero
+//                        }
+//                    }
+//            )
+////            .animation(.spring(), value: dragAmount)
+
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double(num) / 20),
+                               value: dragAmount)
+            }
         }
-        .frame(width: 200, height: 200)
-        .background(enabled ? .blue : .red)
-//        .animation(.default, value: enabled)
-        .animation(nil, value: enabled)
-        .foregroundColor(.white)
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
