19885c7d18ad317ceb9fb35eda13df5c081b89eb
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-04-10 16:18:06 -0500

    project 7: showing how to use onDelete()

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,38 +7,44 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    
-    let name: String
-    
-    var body: some View {
-        Spacer()
-        Spacer()
-        
-        Text("Hellow, \(name)!")
-        
-        Spacer()
-        
-        Button("Dismiss") {
-            dismiss()
-        }
-        
-        Spacer()
-        Spacer()
-    }
-}
+
 
 struct ContentView: View {
-    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
     
     var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                    /*
+                     could also just do
+                     List(numbers, id: \.self) {
+                     Text("Row \($0)")
+                     }
+                     here but .onDelete needs the 'ForEach" to work
+                     */
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .navigationTitle("onDelete")
+            .toolbar {
+                EditButton()
+            }
         }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "@twostraws")
-        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
