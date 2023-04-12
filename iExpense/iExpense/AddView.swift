d3d3bf11ab07f68bfef5cec7279b06f693c23c40
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-04-12 14:13:34 -0500

    project 7: add a new view to add an expense and share expenses with ContentView

---
+++ iExpense/iExpense/AddView.swift
@@ -0,0 +1,40 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Cory Steers on 4/12/23.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    let types = [ "Business", "Personal" ]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id:\.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
