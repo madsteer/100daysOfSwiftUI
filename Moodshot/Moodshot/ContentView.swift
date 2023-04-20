fff635809d3de670610e21849566f05e6e24d686
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-04-20 15:57:58 -0500

    project 8: ScrollViews - horizontal scroll

--- Moodshot/Moodshot/ContentView.swift
+++ Moodshot/Moodshot/ContentView.swift
@@ -21,8 +21,8 @@ struct CustomText: View {
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 10) {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) {
                 ForEach(0..<100) {
                     CustomText("Item \($0)")
                         .font(.title)
