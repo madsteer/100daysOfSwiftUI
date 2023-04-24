2d04dc3555053eb9d4779abdea503d7153343967
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-04-24 14:19:27 -0500

    project 8: grid layout - horizontal adaptive grid layout

--- Moodshot/Moodshot/ContentView.swift
+++ Moodshot/Moodshot/ContentView.swift
@@ -22,8 +22,8 @@ struct ContentView: View {
         GridItem(.adaptive(minimum: 80, maximum: 120))
     ]
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: layout) {
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: layout) {
                 ForEach(0..<1000) {
                     Text("Item \($0)")
                 }
