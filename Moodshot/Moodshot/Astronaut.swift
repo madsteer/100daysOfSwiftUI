6c52beae49eaf7d2a670c10485d8efbfe0f6173f
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-04-24 15:24:31 -0500

    project 8: add file assets to project and code up a clever way to decode json using an extension

---
+++ Moodshot/Moodshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronot.swift
+//  Moodshot
+//
+//  Created by Cory Steers on 4/24/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
