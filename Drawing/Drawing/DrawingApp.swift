4ccc2ea36ba486c3cf31e0523d1875f630a3f82b
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-05-16 13:32:54 -0500

    project 9: initial hello world

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Cory Steers on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
