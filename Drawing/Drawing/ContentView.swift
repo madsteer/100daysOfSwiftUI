2867a346709e4db92889dea4783143dd95e9735b
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-05-23 15:53:09 -0500

    project 9: creative borders and fills - 2

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,35 +7,11 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffset = -20.0
-    var petalWidth = 100.0
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
-        }
-        
-        return path
-    }
-}
-
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
-    
     var body: some View {
-       Text("hello, world!")
-            .frame(width: 300, height: 300)
-            .border(.red, width: 30)
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y 0.25, width: 1, height: 0.5), scale: 0.3), lineWidth: 20)
+            .frame(width: 300, height: 20)
     }
 }
 
