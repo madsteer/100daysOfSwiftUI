31da9c26dd891c12b6fa851e4651f7580ccf6db0
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-05-23 15:53:32 -0500

    project 9: creative borders and fills - 2

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Capsule()
-            .strokeBorder(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y 0.25, width: 1, height: 0.5), scale: 0.3), lineWidth: 20)
+            .strokeBorder(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.3), lineWidth: 20)
             .frame(width: 300, height: 20)
     }
 }
