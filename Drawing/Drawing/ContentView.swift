3221fd982b2337ee63750c9b02b16206b7da8adb
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-06-01 09:19:40 -0500

    project 9: challenge 1: create an arrow Shape using Paths

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,94 +7,54 @@
 
 import SwiftUI
 
-struct Spirograph: Shape {
-    let innerRadius: Int
-    let outerRadius: Int
-    let distance: Int
-    let amount: Double
-    
-    func gcd(_ a: Int, _ b: Int) -> Int {
-        var a = a
-        var b = b
-        
-        while b != 0 {
-            let temp = b
-            b = a % b
-            a = temp
-        }
-        
-        return a
-    }
-    
+struct Arrow: Shape {
     func path(in rect: CGRect) -> Path {
-        let divisor = gcd(innerRadius, outerRadius)
+        let arrowTip = CGPoint(x: rect.midX, y: rect.minY)
+        let arrowRightTip = CGPoint(
+            x: rect.maxX - (rect.midX / 2),
+            y: rect.midY - (rect.midY / 2)
+        )
+        let arrowLeftTip = CGPoint(x: rect.midX / 2, y: rect.midY / 2)
         
-        let outerRadius = Double(self.outerRadius)
-        let innerRadius = Double(self.innerRadius)
-        let distance = Double(self.distance)
-        
-        let difference = innerRadius - outerRadius
-        let endPoint = ceil(2 * Double.pi * outerRadius / Double(divisor)) * amount
-        
+        let firstRectangleCorner = CGPoint(
+            x: rect.midX - (rect.midX / 4),
+            y: rect.midY / 2
+        )
+        let secondRectangleCorner = CGPoint(
+            x: rect.midX + (rect.midX / 4),
+            y: rect.midY / 2
+        )
+        let thirdRectangleCorner = CGPoint(
+            x: rect.midX + (rect.midX / 4),
+            y: rect.midY + (rect.midY / 4)
+            )
+        let fourthRectangleCorner = CGPoint(
+            x: rect.midX - (rect.midX / 4),
+            y: rect.midY + (rect.midY / 4)
+            )
+
         var path = Path()
         
-        for theta in stride(from: 0, through: endPoint, by: 0.01) {
-            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
-            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
-            
-            x += rect.width / 2
-            y += rect.width / 2
-            
-            if theta == 0 {
-                path.move(to: CGPoint(x: x, y: y))
-            } else {
-                path.addLine(to: CGPoint(x: x, y: y))
-            }
-        }
-                
+        path.move(to: arrowTip)
+        path.addLine(to: arrowRightTip)
+        path.addLine(to: arrowLeftTip)
+        path.addLine(to: arrowTip)
+        
+        path.move(to: firstRectangleCorner)
+        path.addLine(to: secondRectangleCorner)
+        path.addLine(to: thirdRectangleCorner)
+        path.addLine(to: fourthRectangleCorner)
+
         return path
     }
 }
 
+
 struct ContentView: View {
-    @State private var innerRadius = 125.0
-    @State private var outerRadius = 75.0
-    @State private var distance = 25.0
-    @State private var amount = 1.0
-    @State private var hue = 0.6
-    
+   
     var body: some View {
-        VStack(spacing: 0) {
-            Spacer()
-            
-            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
-                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
-                .frame(width: 300, height: 300)
-            
-            Spacer()
-            
-            Group {
-                Text("Inner radius: \(Int(innerRadius))")
-                Slider(value: $innerRadius, in: 10...150, step: 1)
-                    .padding([ .horizontal, .bottom])
-                
-                Text("Outer radius: \(Int(outerRadius))")
-                Slider(value: $outerRadius, in: 10...150, step: 1)
-                    .padding([ .horizontal, .bottom])
-                
-                Text("Distance: \(Int(distance))")
-                Slider(value: $distance, in: 1...150, step: 1)
-                    .padding([ .horizontal, .bottom])
-                
-                Text("Amount: \(amount, format: .number.precision(.fractionLength(2)))")
-                Slider(value: $amount)
-                    .padding([ .horizontal, .bottom])
-                
-                Text("Color")
-                Slider(value: $hue)
-                    .padding(.horizontal)
-            }
-        }
+        Arrow()
+            .frame(width: 300, height: 600)
     }
 }
 
