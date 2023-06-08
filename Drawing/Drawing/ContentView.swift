854879349ef7183aa571f7e14bcca82d59515021
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-06-08 15:25:25 -0500

    project 9: challenge 3 - make a ColorCyclingRectangle

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,59 +7,58 @@
 
 import SwiftUI
 
-struct Arrow: Shape {
-    func path(in rect: CGRect) -> Path {
-        let arrowTip = CGPoint(x: rect.midX, y: rect.minY)
-        let arrowRightTip = CGPoint(
-            x: rect.maxX - (rect.midX / 2),
-            y: rect.midY - (rect.midY / 2)
-        )
-        let arrowLeftTip = CGPoint(x: rect.midX / 2, y: rect.midY / 2)
+struct ColorCyclingRectangle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps, id: \.self) { value in
+                Rectangle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
         
-        let rectangleX: CGFloat = rect.midX - (rect.midX / 4)
-        let rectangleY: CGFloat =  rect.midY / 2
-        let rectangleWidth = rect.midX + (rect.midX / 4) - rectangleX
-        let rectangleHeight = rect.midY + (rect.midY / 4) - rectangleY
-
-        var path = Path()
+        if targetHue > 1 {
+            targetHue -= 1
+        }
         
-        path.move(to: arrowTip)
-        path.addLine(to: arrowRightTip)
-        path.addLine(to: arrowLeftTip)
-        path.addLine(to: arrowTip)
-        
-        let cgRect: CGRect = CGRect(
-            x: rectangleX,
-            y: rectangleY,
-            width: rectangleWidth,
-            height: rectangleHeight
-        )
-        
-        path.addRect(cgRect)
-        
-        return path
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
-
 struct ContentView: View {
-    @State private var lineWidth: CGFloat = 10.0
-   
-    var animatableData: Double {
-        get { lineWidth }
-        set { lineWidth = newValue }
-    }
+    @State private var colorCycle = 0.0
     
     var body: some View {
-        Arrow()
-            .stroke(.red, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
-            .frame(width: 300, height: 600)
-            .onTapGesture {
-                withAnimation(.easeInOut(duration: 3)) {
-                    lineWidth = Double.random(in: 1...20)
-                    print("I'm here.")
-                }
-            }
+        VStack {
+            Spacer()
+            
+            ColorCyclingRectangle(amount: colorCycle)
+                .frame(width: 300, height: 300)
+            
+            Spacer()
+            
+            Slider(value: $colorCycle)
+            
+            Spacer()
+        }
     }
 }
 
