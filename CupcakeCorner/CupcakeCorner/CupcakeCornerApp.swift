e0af516a19ce11ef2178976d33d7d3041b5357d5
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-06-14 16:10:18 -0500

    project 10: adding codable conformance for @Published properties

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Cory Steers on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
