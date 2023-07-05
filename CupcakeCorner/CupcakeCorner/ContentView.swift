aac70c60381a44c36ad38b026da37f8d7383296e
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-07-05 16:23:54 -0500

    project 10: examples loading an image from a remove server using AsyncImage

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,50 +7,41 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
 
 struct ContentView: View {
-    @State private var results = [Result]()
     
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                
-                Text(item.collectionName)
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) // works
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3) // works
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"))
+//            .resizable() // won't compile
+//            .frame(width: 200, height: 200) // won't have any affect
+        
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+//            image
+//                .resizable()
+//                .scaledToFit()
+//        } placeholder: {
+//            ProgressView()
+//        }
+//        .frame(width: 200, height: 200)
+        
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
             }
         }
-        .task {
-            await loadData()
-        }
+        .frame(width: 200, height: 200)
+        
     }
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
-        
-    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
