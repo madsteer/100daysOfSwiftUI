d01374c077dfa53ac6571511f705024804b1fec0
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-07-10 22:21:51 -0500

    project 11: creating a custom component with @Binding

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Cory Steers on 7/10/23.
+//
+
+import SwiftUI
+
+struct PushButton: View {
+    let title: String
+    @Binding  var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(
+            LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom)
+        )
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct ContentView: View {
+    @State private var rememberMe = false
+    
+    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+        
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
