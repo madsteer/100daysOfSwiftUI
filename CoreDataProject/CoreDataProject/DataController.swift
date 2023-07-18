7d510bb9f05091db2c46a70f0053261ccf5e089c
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-07-18 17:30:25 -0500

    project 12: set up initial code

---
+++ CoreDataProject/CoreDataProject/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Cory Steers on 7/18/23.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
