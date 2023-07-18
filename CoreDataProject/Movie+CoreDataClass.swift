e53acd07252bf92eddcbc6244a275d0b212757ef
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-07-18 17:55:51 -0500

    project 12: creating NSManagedObject subclasses

---
+++ CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Cory Steers on 7/18/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
