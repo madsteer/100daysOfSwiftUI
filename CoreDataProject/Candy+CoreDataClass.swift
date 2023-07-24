7c0f1c6bb5d6e9634fb76d0fadc1ec1972948e2e
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-07-24 13:56:08 -0500

    project 12: one-to-many relationships with Core Data, SwiftUI, and @FetchRequest

---
+++ CoreDataProject/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Cory Steers on 7/24/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Cory Steers on 7/24/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
