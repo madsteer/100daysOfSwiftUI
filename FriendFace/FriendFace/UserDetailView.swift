970292b97f4527fc7b61eb93626197624f939e27
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-08-02 13:52:28 -0500

    day 60, milestone project: friend face app - add detail page showing friends that is tapable from main list

---
+++ FriendFace/FriendFace/UserDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  UserDetailView.swift
+//  FriendFace
+//
+//  Created by Cory Steers on 7/27/23.
+//
+
+import SwiftUI
+
+struct UserDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserDetailView()
+    }
+}
