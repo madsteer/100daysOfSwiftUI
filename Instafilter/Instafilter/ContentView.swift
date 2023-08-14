f7b127fbd395c7588c7aa1deae26f7293f977673
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-08-14 12:21:28 -0500

    project 13: integrating Core Image with SwiftUI - simple Image view

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,29 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
     
     var body: some View {
         VStack {
 //            Image(systemName: "globe")
 //                .imageScale(.large)
 //                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-                .frame(width: 300, height: 300)
-                .background(backgroundColor)
-                .onTapGesture {
-                    showingConfirmation = true
-                }
-                .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                    Button("Red") { backgroundColor = .red }
-                    Button("Green") { backgroundColor = .green }
-                    Button("Blue") { backgroundColor = .blue }
-                    Button("Cancel", role: .cancel) { }
-                } message: {
-                    Text("select a new color")
-                }
+            image?
+                .resizable()
+                .scaledToFit()
         }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        image = Image("Example")
     }
 }
 
