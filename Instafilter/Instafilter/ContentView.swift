fd8fa481c7d13ed25ffce1ed6cf260f162f41f8a
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-08-17 13:42:36 -0500

    project 13: wrapping a UIViewController in a SwiftUI View

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -11,70 +11,24 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var image: Image?
+    @State private var showingImagePicker = false
     
     var body: some View {
         VStack {
 //            Image(systemName: "globe")
 //                .imageScale(.large)
 //                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
             image?
                 .resizable()
                 .scaledToFit()
+            
+            Button("Select Image") {
+                showingImagePicker = true
+            }
         }
-        .onAppear(perform: loadImage)
-    }
-    
-    func loadImage() {
-        guard let inputImage = UIImage(named: "Example") else { return }
-        let beginImage = CIImage(image: inputImage)
-        
-//        let currentFilter = CIFilter.sepiaTone()
-//        currentFilter.inputImage = beginImage
-//        currentFilter.intensity = 1
-        
-//        let currentFilter = CIFilter.pixellate()
-//        currentFilter.inputImage = beginImage
-//        currentFilter.scale = 25
-        
-//        let currentFilter = CIFilter.crystallize()
-//        currentFilter.inputImage = beginImage
-//        currentFilter.radius = 50
-        
-//        let currentFilter = CIFilter.twirlDistortion()
-//        currentFilter.inputImage = beginImage
-//        currentFilter.radius = 1000
-//        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
-        
-//        let currentFilter = CIFilter.sepiaTone()
-//        let currentFilter = CIFilter.pixellate()
-//        let currentFilter = CIFilter.crystallize()
-        let currentFilter = CIFilter.twirlDistortion()
-        currentFilter.inputImage = beginImage
-        
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys // older API allows for more flexability
-        
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
-        }
-        
-        if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(amount + 200, forKey: kCIInputRadiusKey)
-        }
-        
-        if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
-        }
-                
-        currentFilter.radius = 1000
-        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
-
-        guard let outputImage = currentFilter.outputImage else { return }
-        
-        let context = CIContext()
-        if let cgImg = context.createCGImage(outputImage, from: outputImage.extent) {
-            let uiImage = UIImage(cgImage: cgImg)
-            image = Image(uiImage: uiImage)
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker()
         }
     }
 }
