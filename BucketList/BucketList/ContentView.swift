f604557f84610ad677b6b0638ed5298f9e5cedf8
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-08-21 15:16:25 -0500

    project 14: writing data to the documents directory

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -7,34 +7,33 @@
 
 import SwiftUI
 
-struct User: Identifiable, Comparable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-    
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
-
 struct ContentView: View {
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister")
-    ].sorted()
-    
     var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
-//            Text("Hello, world!")
-//        }
-//        .padding()
-        List(users) { user in
-            Text("\(user.firstName) \(user.lastName)")
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+                .onTapGesture {
+                    let str = "Test Message"
+                    let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                    
+                    do {
+                        try str.write(to: url, atomically: true, encoding: .utf8)
+                        
+                        let input = try String(contentsOf: url)
+                        print(input)
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
         }
+        .padding()
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 }
 
