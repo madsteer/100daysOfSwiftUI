6aecaeac7e17aa8c83427d4da1d38449d661441d
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-08-31 13:31:10 -0500

    project 15 - accessibility: Identifying views with useful labels

---
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  AccessibilitySandbox
+//
+//  Created by Cory Steers on 8/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+        
+        Image(pictures[selectedPicture])
+            .resizable()
+            .scaledToFit()
+            .onTapGesture {
+                selectedPicture = Int.random(in: 0...3)
+            }
+            .accessibilityLabel(labels[selectedPicture]) // we can do a better job of describing the images than their cryptic names
+            .accessibilityAddTraits(.isButton) // because of onTapGesture it's also a button
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
