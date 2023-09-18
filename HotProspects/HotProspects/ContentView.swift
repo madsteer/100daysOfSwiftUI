cb0de3954680f0db3df4134647e873dcea4c56cd
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-09-18 16:00:04 -0500

    project 16: scheduling local notifications

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
@@ -17,23 +18,29 @@ struct ContentView: View {
 //        }
 //        .padding()
         
-        List {
-            Text("Taylor Swift")
-                .swipeActions {
-                    Button(role: .destructive) {
-                        print("Deleting")
-                    } label: {
-                        Label("Delete", systemImage: "minus.circle")
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
                     }
                 }
-                .swipeActions(edge: .leading) {
-                    Button {
-                        print("Pinning")
-                    } label: {
-                        Label("Pin", systemImage: "pin")
-                    }
-                    .tint(.orange)
-                }
+            }
+            
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the dogs"
+                content.subtitle = "They look hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
         }
     }
 }
