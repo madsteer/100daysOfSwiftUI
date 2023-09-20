db1a1401a68295da30d78ad48a5bfc0e0f036d53
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-09-20 15:19:01 -0500

    project 16: building the tab bar

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -5,27 +5,29 @@
 //  Created by Cory Steers on 9/13/23.
 //
 
-import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    let possibleNumbers = Array(1...60)
     var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-//            Text("Hello, world!")
-//        }
-//        .padding()
-        
-        Text(results)
-    }
-    
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.joined(separator: ",")
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
     }
 }
 
---
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,38 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Cory Steers on 9/20/23.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    let filter: FilterType
+    
+    var body: some View {
+        NavigationView {
+            Text("Hello, world!")
+                .navigationTitle(title)
+        }
+    }
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+}
+
+#Preview {
+    ProspectsView(filter: .none)
+}
