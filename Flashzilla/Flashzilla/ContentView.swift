a08dcaa454457d870bf6cd0e58a7f78cb0be2294
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-09-28 11:29:17 -0500

    project 17: how to use gestures in SwiftUI - scale effect magnification gesture

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,19 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentAmount = 0.0
+    @State private var finalAmount = 1.0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-                .onLongPressGesture(minimumDuration: 1) {
-                    print("Long pressed!")
-                } onPressingChanged: { inProgress in
-                    print("In progress: \(inProgress)")
-                }
+//                .onLongPressGesture(minimumDuration: 1) {
+//                    print("Long pressed!")
+//                } onPressingChanged: { inProgress in
+//                    print("In progress: \(inProgress)")
+//                }
         }
         .padding()
+        .scaleEffect(finalAmount + currentAmount)
+        .gesture(
+            MagnificationGesture()
+                .onChanged { amount in
+                    print("executing on changed")
+                    currentAmount = amount - 1
+                }
+                .onEnded { amount in
+                    print("executing on ended")
+                    finalAmount += currentAmount
+                    currentAmount = 0
+                }
+        )
+
     }
 }
 
