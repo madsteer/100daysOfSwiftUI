2040a1b9edbb8c38028a72e80da83b596c9266cf
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-09-28 12:15:33 -0500

    project 17: how to use gestures in SwiftUI - multiple gesture priority and sequencing

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,34 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentAmount = Angle.zero
-    @State private var finalAmount = Angle.zero
+    @State private var offset = CGSize.zero
+    @State private var isDragging = false
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-//                .onLongPressGesture(minimumDuration: 1) {
-//                    print("Long pressed!")
-//                } onPressingChanged: { inProgress in
-//                    print("In progress: \(inProgress)")
+        let dragGesture = DragGesture()
+            .onChanged { value in
+                offset = value.translation
+            }
+            .onEnded { _ in
+                withAnimation {
+                    offset = .zero
+                    isDragging = false
+                }
+            }
+        
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    isDragging = true
+                }
+            }
+        
+        let combined = pressGesture.sequenced(before: dragGesture)
+        
+        Circle()
+            .fill(.red)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
+        
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//                .onTapGesture {
+//                    print("Text Tapped!")
 //                }
-        }
-        .padding()
-        .rotationEffect(finalAmount + currentAmount)
-        .gesture(
-            RotationGesture()
-                .onChanged { angle in
-                    currentAmount = angle
-                }
-                .onEnded { angle in
-                    finalAmount += currentAmount
-                    currentAmount = .zero
-                }
-        )
-
+//        }
+//        .padding()
+////        .highPriorityGesture( // causes outer geture to take priority over inner gesture
+//        .simultaneousGesture( // causes both getures to fire
+//            TapGesture()
+//                .onEnded {
+//                    print("VStack tapped")
+//                }
+//        )
     }
 }
 
