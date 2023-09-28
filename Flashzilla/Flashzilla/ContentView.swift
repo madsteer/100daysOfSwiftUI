4de83c091e2332d0331c5c41fe80b0c674a605c8
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-09-28 13:53:06 -0500

    project 17: make vibrations with core haptics

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -5,57 +5,67 @@
 //  Created by Cory Steers on 9/28/23.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
+    @State private var engine: CHHapticEngine?
     
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = value.translation
-            }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
-                }
-            }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .onAppear(perform: prepareHaptics)
+//        .onTapGesture(perform: simpleSuccess)
+        .onTapGesture(perform: complexSuccess)
+    }
+
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
         
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
-                }
-            }
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the enging: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess(){
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
         
-        let combined = pressGesture.sequenced(before: dragGesture)
+        var events = [CHHapticEvent]()
         
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
-        
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-//            Text("Hello, world!")
-//                .onTapGesture {
-//                    print("Text Tapped!")
-//                }
-//        }
-//        .padding()
-////        .highPriorityGesture( // causes outer geture to take priority over inner gesture
-//        .simultaneousGesture( // causes both getures to fire
-//            TapGesture()
-//                .onEnded {
-//                    print("VStack tapped")
-//                }
-//        )
+        for i in stride(from: 0, through: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+
+        for i in stride(from: 1, through: 0, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern \(error.localizedDescription)")
+        }
     }
 }
 
