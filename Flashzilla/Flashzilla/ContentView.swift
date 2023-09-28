dade0f69fc5da22e5fc7b8adeadc0d19660a7025
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-09-28 17:05:31 -0500

    project 17: Disabling user interactivity with allowsHitTesting()

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -5,67 +5,44 @@
 //  Created by Cory Steers on 9/28/23.
 //
 
-import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @State private var engine: CHHapticEngine?
-    
     var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello")
+            Spacer().frame(height: 100)
+            Text("World")
         }
-        .padding()
-        .onAppear(perform: prepareHaptics)
-//        .onTapGesture(perform: simpleSuccess)
-        .onTapGesture(perform: complexSuccess)
-    }
-
-    func simpleSuccess() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.success)
-    }
-    
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        do {
-            engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the enging: \(error.localizedDescription)")
+        .contentShape(Rectangle()) // makes whole Vstack tappable including Spacer area
+        .onTapGesture {
+            print("Vstack tapped!")
         }
-    }
-    
-    func complexSuccess(){
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        var events = [CHHapticEvent]()
-        
-        for i in stride(from: 0, through: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-
-        for i in stride(from: 1, through: 0, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
-        }
-
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern \(error.localizedDescription)")
-        }
+//        ZStack {
+//            Rectangle()
+//                .fill(.blue)
+//                .frame(width: 300, height: 300)
+//                .onTapGesture {
+//                    print("Rectangle tapped!")
+//                }
+//            
+//            Circle()
+//                .fill(.red)
+//                .frame(width: 300, height: 300)
+//                .contentShape(Rectangle()) // whole rectangle is circle tapped
+//                .onTapGesture {
+//                    print("Circle tapped!")
+//                }
+//                .allowsHitTesting(false) // whole rectangle is rectangle tapped
+//        }
     }
 }
 
