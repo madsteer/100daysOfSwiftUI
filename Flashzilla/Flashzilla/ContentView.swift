f5d1cb6b16593ec2be32dcc9794eca62879133da
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-10-04 20:29:45 -0500

    project 17: Supporting specific accessibility needs with SwiftUI - reduce motion

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -7,29 +7,41 @@
 
 import SwiftUI
 
+func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+    if UIAccessibility.isReduceMotionEnabled {
+        return try body()
+    } else {
+        return try withAnimation(animation, body)
+    }
+}
+
 struct ContentView: View {
-    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\..accessibilityReduceMotion) var reduceMotion
+    @State private var scale = 1.0
     
     var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-//            Text("Hello, world!")
-//        }
-//        .padding()
-        
-        HStack {
-            if differentiateWithoutColor {
-                Image(systemName: "checkmark.circle")
-            }
-            
-            Text("Success")
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
         .padding()
-        .background(differentiateWithoutColor ? .black : .green)
-        .foregroundColor(.white)
-        .clipShape(Capsule())
+        .scaleEffect(scale)
+        .onTapGesture {
+//            if reduceMotion {
+//                scale += 1.5
+//            } else {
+//                withAnimation {
+//                    scale *= 1.5
+//                }
+//            }
+            withOptionalAnimation {
+                scale *= 1.5
+            }
+        }
+        
+        
     }
 }
 
