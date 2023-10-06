8c34299e8862c8e75652279bb15113b18edb9f3d
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-10-06 14:02:30 -0500

    project 17: Designing a single card view

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -7,30 +7,9 @@
 
 import SwiftUI
 
-func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
-}
-
 struct ContentView: View {
-    @Environment(\..accessibilityReduceTransparency) var reduceTransparency
-    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-        .background(reduceTransparency ? .black : .black.opacity(0.5))
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        
-        
+        CardView(card: Card.example)
     }
 }
 
