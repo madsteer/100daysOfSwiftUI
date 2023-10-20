a3343643ac91662794841ec6fcb209540ae33b4f
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-10-20 13:36:26 -0500

    project 18: absolute positioning for swiftUI views

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -20,33 +20,15 @@ extension VerticalAlignment {
 
 struct ContentView: View {
     var body: some View {
-        //        VStack {
-        //            Image(systemName: "globe")
-        //                .imageScale(.large)
-        //                .foregroundStyle(.tint)
-        //            Text("Live long and prosper!")
-        //                .frame(width: 300, height: 300, alignment: .topLeading)
-        //                .offset(x: 50, y: 50)
-        //        }
-        //        .padding()
-        
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@madsteer")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image("apollo13")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-                
-            }
-            
-            VStack {
-                Text("Full name:")
-                Text("CORY STEERS")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
-            }
-        }
+                VStack {
+                    Image(systemName: "globe")
+                    Text("Live long and prosper!")
+                }
+                .padding()
+//                .background(.red)
+//                .position(x: 100, y: 100)
+                .offset(x: 100, y: 100)
+                .background(.red)
     }
 }
 
