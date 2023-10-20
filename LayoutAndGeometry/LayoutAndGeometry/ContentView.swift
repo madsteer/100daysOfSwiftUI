72023574015a7b65c9dc3fc9e32bc889e29cb052
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-10-20 16:09:39 -0500

    project 18 - challenge 1: make the views fade out to 0 opacity as they near the top of the view

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -11,40 +11,18 @@ struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
     
     var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//            Text("Hello, world!")
-//        }
-//        .padding()
-        
-//        GeometryReader { fullView in
-//            ScrollView {
-//                ForEach(0..<50) { index in
-//                    GeometryReader { geo in
-//                        Text("Row # \(index)")
-//                            .font(.title)
-//                            .frame(maxWidth: .infinity)
-//                            .background(colors[index % 7])
-//                        //                        .rotation3DEffect(.degrees(geo.frame(in: .global).minY / 5), axis: (x: 0, y: 1, z: 0))
-//                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
-//                    }
-//                    .frame(height: 40)
-//                }
-//            }
-//        }
-        
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                ForEach(1..<20) { number in
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
                     GeometryReader { geo in
-                        Text("Number \(number)")
-                            .font(.largeTitle)
-                            .padding()
-                            .background(.red)
-                            .rotation3DEffect(.degrees(-geo.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
-                            .frame(width: 200, height: 200)
+                        Text("Row #\(index)")
+                            .font(Font.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity((1 * geo.frame(in: .global).origin.y / 200.0) < 0.5 ? 0.5 : (1 * geo.frame(in: .global).origin.y / 200.0) )
                     }
-                    .frame(width: 200, height: 200)
+                    .frame(height: 40)
                 }
             }
         }
