98df9c90f71cd7ad4996da0e208125f67aaca976
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-10-20 14:43:44 -0500

    project 18: ScrollView effects using GeometryReader

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,58 +7,47 @@
 
 import SwiftUI
 
-struct OuterView: View {
-    var body: some View {
-        VStack {
-            Text("top")
-            InnerView()
-                .background(.green)
-            Text("bottom")
-        }
-    }
-}
-
-struct InnerView: View {
-    var body: some View {
-        HStack {
-            Text("left")
-            
-            GeometryReader { geo in
-                    Text("Center")
-                    .background(.blue)
-                    .onTapGesture {
-                        print("Global center is \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
-                        print("Local center is \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
-                        print("Custom center is \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
-                    }
-            }
-            .background(.orange)
-            
-            Text("right")
-        }
-    }
-}
-
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
 //        VStack {
-//            GeometryReader { geo in
-//                VStack {
-//                    Image(systemName: "globe")
-//                    Text("Live long and prosper!")
-//                }
-//                .padding()
-//                .frame(width: geo.size.width * 0.9)
-//                .background(.red)
-//            }
-//            .background(.green)
-//            
-//            Text("more text")
-//                .background(.blue)
+//            Image(systemName: "globe")
+//            Text("Hello, world!")
 //        }
-        OuterView()
-            .background(.red)
-            .coordinateSpace(name: "Custom")
+//        .padding()
+        
+//        GeometryReader { fullView in
+//            ScrollView {
+//                ForEach(0..<50) { index in
+//                    GeometryReader { geo in
+//                        Text("Row # \(index)")
+//                            .font(.title)
+//                            .frame(maxWidth: .infinity)
+//                            .background(colors[index % 7])
+//                        //                        .rotation3DEffect(.degrees(geo.frame(in: .global).minY / 5), axis: (x: 0, y: 1, z: 0))
+//                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+//                    }
+//                    .frame(height: 40)
+//                }
+//            }
+//        }
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { number in
+                    GeometryReader { geo in
+                        Text("Number \(number)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .rotation3DEffect(.degrees(-geo.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                            .frame(width: 200, height: 200)
+                    }
+                    .frame(width: 200, height: 200)
+                }
+            }
+        }
     }
 }
 
