f182b9d7e3b3f7f1d47492b70760ec273f3762eb
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-10-24 16:45:59 -0500

    project 19: user alert() and sheet() with optionals

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,29 +7,36 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
 struct ContentView: View {
+    @State private var selectedUser: User? = nil
+    @State private var isShowingUser = false
+    
     var body: some View {
-//            VStack {
-//                Image(systemName: "globe")
-//                    .imageScale(.large)
-//                    .foregroundStyle(.tint)
-//                Text("Hello, world!")
-//            }
-//            .padding()
-            
-        NavigationView {
-            NavigationLink {
-                Text("New Secondary")
-            } label: {
-                Text("Hellow, world!")
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
             }
-            .navigationTitle("Primary")
-            
-            Text("Secondary")
-            
-            
-            Text("Tertiary")
-        }
+            .padding()
+            .onTapGesture {
+                selectedUser = User()
+                isShowingUser.toggle()
+            }
+//            .sheet(item: $selectedUser) { user in
+//                Text(user.id)
+//            }
+//            .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser) { user in
+//                Button(user.id) { }
+//            }
+//            .alert("Welcome", isPresented: $isShowingUser) {
+//                Button("OK") { }
+//            }
+            .alert("Welcome", isPresented: $isShowingUser) { } // same as above.  perfect for simple alerts
     }
 }
 
